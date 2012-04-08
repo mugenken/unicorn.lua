1c7d6a9d4a92ea64a7997862d40cdfbf43f3f92d
Mugen Kenichi <mugen.kenichi@uninets.eu>
2012-04-08 20:47:33 +0200

    added cli.lua

---
+++ lib/cli.lua
@@ -0,0 +1,112 @@
+package.cpath = '../lib/?/?.so;' .. package.cpath
+package.path = '../lib/?/?.lua;' .. package.path
+
+module (..., package.seeall)
+
+require ('utils')
+require ('proc')
+require ('dumper')
+
+username = nil
+group = nil
+config = nil
+uid = nil
+ptable = nil
+
+local sig = {
+    USR2 = 12,
+    WINCH = 28,
+    QUIT = 3,
+    TTIN = 21,
+    TTOU = 22,
+}
+
+function cli:new (obj)
+    obj = obj or {}
+    setmetatable(obj, self)
+    self.__index = self
+
+    self.ptable = proc.build_ptable()
+
+    if obj.username == nil then
+        self.username = 'nobody'
+    else
+        self.username = obj.username
+    end
+
+    if obj.group == nil then
+        self.group = self.username
+    end
+
+    if obj.config == nil then
+        self.config = 'unicorn.rb'
+    end
+
+    self.uid = utils.getuid(self.username)
+
+    return obj
+end
+
+function start ()
+    return true
+end
+
+function restart ()
+    return true
+end
+
+function reload ()
+    return true
+end
+
+function cli:stop ()
+    return true
+end
+
+function cli:add_worker (num)
+    num = num or 1
+
+    if num < 0 then
+        return false
+    end
+
+    for pid, _ in pairs(self.ptable[self.uid .. '']) do
+        for i = 1, num do
+            utils.kill(pid, sig['TTIN'])
+            utils.usleep(500)
+        end
+    end
+
+end
+
+function cli:remove_worker (num)
+    num = num or 1
+
+    if num < 0 then
+        return false
+    end
+
+    for pid, children in pairs(self.ptable[self.uid .. '']) do
+        local num_workers = #children
+        if num > num_workers then
+            num = num_workers - 1
+        end
+
+        if num <= 0 then
+            return true
+        end
+
+        for i = 1, num do
+            utils.kill(pid, sig['TTOU'])
+            utils.usleep(500)
+        end
+
+        return true
+    end
+
+end
+
+function cli:refresh ()
+    self.ptable = proc.build_ptable()
+end
+
